0f22fb18fdf366784f519ec2dc3fe3389a2b0599
Dr-Coconut <abhayuthappa2002@gmail.com>
2025-09-03 00:35:08 +0530

    js 6-11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if ((a<=0||!(Number.isInteger(a)))||(b<=0||!(Number.isInteger(b)))){
+        return "ERROR";
+    }
+    let min;
+    let max;
+    if (a<=b){
+        min=a;
+        max=b;
+    }
+    else{
+        min=b;
+        max=a;
+    }
+    let sum=0;
+    for(;min<=max;min++){
+        sum+=min;
+    }
+    return sum;
 };
 
 // Do not edit below this line
